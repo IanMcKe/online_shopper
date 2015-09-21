aeb44077e59be44053a1205570549fbfe53249c3
Ian McKenney & Logan Wu <student@epicodus.com>
2015-09-21 15:54:36 -0700

    add addTotal function

--- js/ShopperController.js
+++ js/ShopperController.js
@@ -10,4 +10,28 @@ onlineShopper.controller('ShopperController', function ShopperController($scope)
     var index = $scope.items.indexOf(item);
     $scope.items.splice(index, 1);
   };
+
+  $scope.cart = [];
+  $scope.shippingCost = 0;
+  //using $scope.totalCost = 0 instead of var total = 0 causes a bug for some reason
+    //$scope.totalCost = 0
+  $scope.addToCart = function(item) {
+    for(var i = 0; i < $scope.cart.length)
+    $scope.cart.push(item);
+  };
+
+  $scope.deleteFromCart = function(item) {
+    var index = $scope.cart.indexOf(item);
+    $scope.cart.splice(index, 1);
+  };
+
+  // $scope.shippingCost
+  $scope.addTotal = function() {
+    var total = 0;
+    for(var i = 0; i < $scope.cart.length; i++) {
+      total += $scope.cart[i].price;
+    }
+    total += $scope.shippingCost;
+    return total;
+  };
 });
